ba7bfb999ce730184448984586661412ec8cfcf9
Erick-Baco <jesusbaco542@gmail.com>
2022-11-28 16:26:45 -0600

    cambio de variable a float

--- suma y resta.cpp
+++ suma y resta.cpp
@@ -2,9 +2,11 @@
 
 int main()
 {
-	int n1,n2,resultado;
+	float n1,n2,resultado;
 	
 	printf("\n\t programa que suma y resta dos numeros");
 	
 	printf("\n\t ingresa: \n\n\t -1 para suma \n\t -2 para resta");
+	
+	
 }
