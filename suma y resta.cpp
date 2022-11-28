4229ed87384710b65b7c457710431f0e638d155d
Erick-Baco <jesusbaco542@gmail.com>
2022-11-28 16:23:40 -0600

    estructura inicial del programa

---
+++ suma y resta.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+	int n1,n2,resultado;
+	
+	printf("\n\t programa que suma y resta dos numeros");
+	
+	printf("\n\t ingresa: \n\n\t -1 para suma \n\t -2 para resta");
+}
