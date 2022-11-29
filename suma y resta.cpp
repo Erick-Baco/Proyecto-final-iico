bf8c68b2c75f6c52a24fdf6f3a99c67231344e99
Erick-Baco <jesusbaco542@gmail.com>
2022-11-29 17:26:04 -0600

    ambos case

--- suma y resta.cpp
+++ suma y resta.cpp
@@ -7,13 +7,15 @@ int main()
 	
 	printf("\n\t programa que suma y resta dos numeros");
 	
-	printf("\n\t ingresa (en may%csculas): \n\n\t -S para suma \n\t -R para resta",163);
+	printf("\n\t ingresa (en may%csculas): \n\n\t -S para suma \n\t -R para resta\n\t",163);
 	opcion=getchar();
 	
 	switch(opcion)
 	{
 		case 'S':
 			
+		break;
+		case 'R':
 		break;
 		
 	}
