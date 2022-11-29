13f10eddd96f3d0d4e6b60fcb135962bbd70cd0b
Erick-Baco <jesusbaco542@gmail.com>
2022-11-29 16:06:10 -0600

    crea menu con switch

--- suma y resta.cpp
+++ suma y resta.cpp
@@ -3,10 +3,18 @@
 int main()
 {
 	float n1,n2,resultado;
+	char opcion;
 	
 	printf("\n\t programa que suma y resta dos numeros");
 	
-	printf("\n\t ingresa: \n\n\t -1 para suma \n\t -2 para resta");
+	printf("\n\t ingresa (en may%csculas): \n\n\t -S para suma \n\t -R para resta",163);
+	getchar(opcion);
+	
+	switch(opcion)
+	{
+		case 'S'
+		
+	}
 	
 	
 }
